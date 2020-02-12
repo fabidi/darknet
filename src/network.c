eea964900e3310814353b15092f10fdb83b7582d
Abidi, Faisal (MONHC-C) <faisal.abidi@hyatt.com>
2020-02-11 23:09:20 -0600

    HCOM-6010: Test commit

--- src/network.c
+++ src/network.c
@@ -217,7 +217,7 @@ void forward_network2(network *netp, int debugmode)
         if(l.delta){
             fill_cpu(l.outputs * l.batch, 0, l.delta, 1);
         }
-//        l.forward(l, net);
+        l.forward(l, net);
 //        net.input = l.output;
 //        if(l.truth) {
 //            net.truth = l.output;
