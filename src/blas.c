dadb253d3b5ec889d683e750e74da4eb71bd96cc
Abidi, Faisal (MONHC-C) <faisal.abidi@hyatt.com>
2020-02-12 09:18:55 -0600

    HCOM-6010: Test commit 4

--- src/blas.c
+++ src/blas.c
@@ -184,7 +184,7 @@ void axpy_cpu(int N, float ALPHA, float *X, int INCX, float *Y, int INCY)
 void scal_cpu(int N, float ALPHA, float *X, int INCX)
 {
     int i;
-    printf("Entering scal_cpu, N: %d, X: %.4f", N, X);
+    printf("Entering scal_cpu, N: %d, X: %.4f\n", N, X);
 //    for(i = 0; i < N; ++i) X[i*INCX] *= ALPHA;
 }
 
