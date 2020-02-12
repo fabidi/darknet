879b4e182f8816a6a385d79f514433f366ee14c3
Abidi, Faisal (MONHC-C) <faisal.abidi@hyatt.com>
2020-02-11 22:47:27 -0600

    HCOM-6010: Test commit

--- examples/detector.c
+++ examples/detector.c
@@ -597,11 +597,11 @@ void test_detector2(char *datacfg, char *cfgfile, char *weightfile, char *filena
     image im = load_image_color(input,0,0);
     if (debugmode)
         printf("Before letterbox_image\n");
-//    image sized = letterbox_image(im, net->w, net->h);
-//    layer l = net->layers[net->n-1];
-//
-//    float *X = sized.data;
-//    time=what_time_is_it_now();
+    image sized = letterbox_image(im, net->w, net->h);
+    layer l = net->layers[net->n-1];
+
+    float *X = sized.data;
+    time=what_time_is_it_now();
 //
 //    network_predict(net, X);
 //
